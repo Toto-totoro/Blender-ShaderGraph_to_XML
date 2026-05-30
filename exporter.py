b5f9d1fc14216a78cdad2da6bc657a03dac39c4d
Laupet <vblaurin@gmail.com>
2026-05-30 18:51:08 +0200

    fixed drowndown list (materials still not loading)

--- exporter.py
+++ exporter.py
@@ -1,13 +1,30 @@
+# Blender ShaderGraph_to_XML
+# Contributor(s): Tom Schäfer (tschaefer.acc@gmail.com) and Laurin von Bergmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
 import bpy
 from bpy_extras.io_utils import ExportHelper
 from bpy.props import StringProperty, EnumProperty
 from bpy.types import Operator
 
 # List of all materials in file
-def get_materials():
+def get_materials_callback(self, context):
     materials = []
     for material in bpy.data.materials:
-        materials.append(material)
+        materials.append(material.name)
     return materials
 
 class ExportShaderGraph(bpy.types.Operator, ExportHelper):
@@ -18,9 +35,37 @@ class ExportShaderGraph(bpy.types.Operator, ExportHelper):
     material_selection = EnumProperty(
         name="Material",
         description="Choose a material to export",
-        # items=get_materials()
-        )
-    
+        items=get_materials_callback
+    )
+
+    def draw(self, context):
+        layout = self.layout
+        layout.label(text="Select Material to Export:")
+        layout.prop(self, "material_selection")
+
+    def invoke(self, context, event):
+        self.filepath = ""
+        return context.window_manager.invoke_props_dialog(self)
+
+    def check(self, context):
+        return True
+
+
+    def execute(self, context):
+        target_filepath = self.filepath
+        chosen_material = self.material_selection
+
+        if chosen_material is None:
+            self.report({'ERROR'}, "No material selected for export.")
+            return {'CANCELLED'}
+
+        if chosen_material == 'ALL':
+            materials_to_export = bpy.data.materials
+        else:
+            materials_to_export = [bpy.data.materials.get(chosen_material)]
+
+        return {'FINISHED'}
+
 def register():
     bpy.utils.register_class(ExportShaderGraph)
 
