86e312a67330792af352e6f70edea49d498f5ad7
Tom <tschaefer.acc@gmail.com>
2026-05-30 15:28:02 +0200

    import fix

--- properties.py
+++ properties.py
@@ -21,7 +21,7 @@ from bpy.types import Scene
 
 # For more information about Blender Properties, visit:
 # <https://blender.org/api/blender_python_api_2_78a_release/bpy.types.Property.html>
-# from bpy.props import BoolProperty
+from bpy.props import BoolProperty
 # from bpy.props import CollectionProperty
 # from bpy.props import EnumProperty
 # from bpy.props import FloatProperty
