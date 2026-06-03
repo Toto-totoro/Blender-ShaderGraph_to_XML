e9840999892dafb9646a0e8e7d2f6bf52bb6a65c
Tom <tschaefer.acc@gmail.com>
2026-06-03 22:30:25 +0200

    imports

--- converter.py
+++ converter.py
@@ -16,7 +16,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import bpy
-import xml.etree.ElementTree as ET
+from lxml import etree as ET
 
 def convert_materials_to_xml(materials: list) -> str:
     # root element
